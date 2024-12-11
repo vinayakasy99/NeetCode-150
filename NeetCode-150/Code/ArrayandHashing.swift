d7cf485e55f0724cc6b40da0365f27f984ddf5e6
Vinayaka S Yattinahalli <vsy.smg@gmail.com>
2024-12-11 23:22:10 +0530

    Array and 2 pointer problems

--- NeetCode-150/Code/ArrayandHashing.swift
+++ NeetCode-150/Code/ArrayandHashing.swift
@@ -11,9 +11,9 @@ class ArrayandHashing: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let str = ["eat","tea","tan","ate","nat","bat"]
-        let test = groupAnagrams(str)
-        print(test)
+        let nums = [1,2,3,4]
+        let result = productExceptSelf(nums)
+        print(result)
     }
     
     // Time: O(N) , Space: O(N)
@@ -75,4 +75,104 @@ class ArrayandHashing: UIViewController {
         
         
     }
+    
+    // Time: O(n⋅logn) || O(N)
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var dict = [Int:Int]()
+        for num in nums{
+            dict[num, default:0] += 1
+        }
+        let sortedDict =  dict.sorted {$0.value > $1.value}
+        var result = [Int]()
+        for i in 0..<k {
+            result.append(sortedDict[i].key)
+        }
+        return result
+    }
+    
+    // Time: O(N) || O(N)
+    func encode(_ strs: [String]) -> String {
+        var result = ""
+        for str in strs {
+            result += str + "😀"
+        }
+        return result
+    }
+    
+    func decode(_ s: String) -> [String] {
+        var words = ""
+        var resArray = [String]()
+        for i in s {
+            if i == "😀" {
+                resArray.append(words)
+                words = ""
+            } else {
+                words.append(i)
+            }
+        }
+        return resArray
+    }
+    
+    // Time Complexity: O(n) || Space Compelxity: O(n)
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var prefix = Array(repeating: 1, count: nums.count)
+        var suffix = Array(repeating: 1, count: nums.count)
+        var result = Array(repeating: 0, count: nums.count)
+        
+        for i in 1 ..< nums.count {
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        }
+        
+        for i in (0 ..< nums.count - 1).reversed() {
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        }
+        
+        for i in 0 ..< result.count {
+            result[i] = prefix[i] * suffix[i]
+        }
+        
+        return result
+    }
+
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        var rows = Array(repeating: Set<Character>(), count: 9)
+        var cols = Array(repeating: Set<Character>(), count: 9)
+        var squares = Array(repeating: Set<Character>(), count: 9)
+        
+        for r in 0..<9 {
+            for c in 0..<9 {
+                let value = board[r][c]
+                if value == "." { continue }
+                
+                let squareIndex = (r / 3) * 3 + (c / 3)
+                
+                if rows[r].contains(value) || cols[c].contains(value) || squares[squareIndex].contains(value) {
+                    return false
+                }
+                
+                rows[r].insert(value)
+                cols[c].insert(value)
+                squares[squareIndex].insert(value)
+            }
+        }
+        
+        return true
+    }
+    
+    // Time Complexity: O(n) || Space Compelxity: O(n)
+    func longestConsecutive(_ nums: [Int]) -> Int {
+           var numSet = Set(nums)
+           var longest = 0
+           for num in nums {
+            if !numSet.contains(num - 1){
+                var length = 0
+                while numSet.contains(num + length) {
+                    length += 1
+                }
+                longest = max(length, longest)
+            }
+           }
+           return longest
+        }
+
 }
--- NeetCode-150/Code/Two-Pointers.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Two-Pointers.swift
-//  NeetCode-150
-//
-//  Created by Vinayaka S Yattinahalli on 03/12/24.
-//
-
