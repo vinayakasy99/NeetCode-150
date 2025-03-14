f6faa9a8431206e072055adcd6812fecdb9cc220
Vinayaka S Yattinahalli <vsy.smg@gmail.com>
2025-03-14 23:32:12 +0530

    Balanced binary tree

---
+++ NeetCode-150/Code/Tree/BalancedBinaryTree.swift
@@ -0,0 +1,32 @@
+//
+//  BalancedBinaryTree.swift
+//  NeetCode-150
+//
+//  Created by Vinayaka S Yattinahalli on 14/03/25.
+//
+import UIKit
+
+//https://leetcode.com/problems/balanced-binary-tree/description/
+//https://neetcode.io/solutions/balanced-binary-tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+class BalancedBinaryTree {
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        
+    }
+}
