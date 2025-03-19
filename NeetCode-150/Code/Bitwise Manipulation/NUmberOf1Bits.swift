588adf10e0bf72771c7efb16bd33b40fb946873b
Vinayaka S Yattinahalli <vsy.smg@gmail.com>
2025-03-19 22:58:45 +0530

    commit

---
+++ NeetCode-150/Code/Bitwise Manipulation/NUmberOf1Bits.swift
@@ -0,0 +1,12 @@
+//
+//  NUmberOf1Bits.swift
+//  NeetCode-150
+//
+//  Created by Vinayaka S Yattinahalli on 19/03/25.
+//
+
+class NUmberOf1Bits {
+    func hammingWeight(_ n: Int) -> Int {
+        
+    }
+}
